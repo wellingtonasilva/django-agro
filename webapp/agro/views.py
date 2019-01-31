ba83b0549e81f0d564e07f228d55a85651580206
wellingt <wellingt@motorola.com>
2019-01-31 14:25:37 -0400

    Correcao de erro ao salvar

--- webapp/agro/views.py
+++ webapp/agro/views.py
@@ -118,7 +118,7 @@ def check_login(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return render(request, 'listagem.html', {})
+        return HttpResponseRedirect('/agro/listagem')
     else:
         return render(request, 'login.html', {})
 
